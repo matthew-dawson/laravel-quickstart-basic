4a6e7d26b243ae4f83d8f206643db014de7ad216
Matthew Dawson <mdawson2099@gmail.com>
2021-02-14 17:19:39 +0000

    Updating the taskExecution ARN within the task definitions

--- aws/create_ecsTaskExecutionRole.sh
+++ aws/create_ecsTaskExecutionRole.sh
@@ -2,6 +2,21 @@
 
 set -eux
 
-aws iam --region eu-west-2 create-role --role-name ecsTaskExecutionRole --assume-role-policy-document file://task-execution-assume-role.json
+ROLENAME=ecsTaskExecutionRole
+ROLEARN=$(aws iam create-role \
+    --region eu-west-2 \
+    --role-name $ROLENAME \
+    --assume-role-policy-document file://task-execution-assume-role.json \
+    | grep 'RoleId' \
+    | awk '{ print $2 }' \
+    | tr -d ',"' )
 
-aws iam --region eu-west-2 attach-role-policy --role-name ecsTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
+for i in {app,db,webserver}; do 
+    sed -i "#executionRoleArn# s#: .*# :\"$ROLEARN\",#" tasks/$i-task.json
+done
+
+# Attach the role policy to the role
+aws iam attach-role-policy \
+    --region eu-west-2 \
+    --role-name $ROLENAME \
+    --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
