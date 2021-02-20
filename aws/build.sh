3ec79d51f97246654ccdf99d052282fa190ce95b
Matthew Dawson <mdawson2099@gmail.com>
2021-02-20 03:34:33 +0000

    Templating (#8)
    * Templating the buildspec definitions
    
    * Tidying up logic and templating

--- aws/build.sh
+++ aws/build.sh
@@ -1,79 +1,37 @@
 #! /bin/bash
 
+set -eux
+
 PROJECT="laravel"
 REGION="eu-west-2"
-#VPCID=''
-#IGWID=''
-#ROUTETABLEID=''
-#SUBNETID0=''
-#SUBNETID1=''
-#NAMESPACEID=''
-#OPERATIONID=''
- 
-set -eux 
+DATAFILE="project.data"
+# AWS cli 2 introduces the default usage of a pager
+export AWS_PAGER=""
 
 init () {
+
+    if [[ ! -e $DATAFILE ]]; then
+        touch $DATAFILE
+        echo "$PROJECT" > $DATAFILE
+    else
+        echo "$DATAFILE EXISTS!"
+        exit 255
+    fi
+
     # Determine AWS account
     ACCOUNT=$(aws sts get-caller-identity \
         | grep 'Account' | awk '{ print $2 }' \
         | tr -d ',"')
-    echo "$ACCOUNT" > account.data
-    echo "$PROJECT" > project.data
 
-    ## Order matters here
-    for i in {app,db,webserver}; do
-        sed "s/<ACCOUNT>/$ACCOUNT/g" tasks/"$i"-task.json.tmpl \
-            > tasks/"$i"-task.json
-        sed -i "s/<PROJECT>/$PROJECT/g" tasks/"$i"-task.json
-    done
-
-    ## Order matters here
-    sed "s/<ACCOUNT>/$ACCOUNT/g" \
-        codeBuild/"$PROJECT"-project.json.tmpl \
-        > codeBuild/"$PROJECT"-project.json
-    sed -i "s/<PROJECT>/$PROJECT/g" \
-        codeBuild/"$PROJECT"-project.json
-
-    sed "s/<ACCOUNT>/$ACCOUNT/g" \
-        codePipeline/create-role-policy.json.tmpl \
-        > codePipeline/create-role-policy.json
-    sed -i "s/<PROJECT>/$PROJECT/g" \
-        codePipeline/create-role-policy.json
-
-    sed "s/<ACCOUNT>/$ACCOUNT/g" \
-        codePipeline/create-pipeline.json.tmpl \
-        > codePipeline/create-pipeline.json
-    sed -i "s/<PROJECT>/$PROJECT/g" \
-        codePipeline/create-pipeline.json
-
-    ## Determine Codestar integration
-    CONNECTIONARN=$(aws codestar-connections \
-        list-connections \
-        | grep 'ConnectionArn' \
-        | awk '{ print $2 }' \
-        | tr -d ',"')
-
-    sed -i "s#<CONNECTIONARN>#$CONNECTIONARN#g" codePipeline/create-pipeline.json
-
-## Determine Codestar integration
-CONNECTIONARN=$(aws codestar-connections \
-    list-connections \
-    | grep 'ConnectionArn' \
-    | awk '{ print $2 }' \
-    | tr -d ',"')
-
-sed -i "s#<CONNECTIONARN>#$CONNECTIONARN#g" codePipeline/create-pipeline.json
+}
 
 create_vpc () {
 
-    local DATAFILE="vpc.data"
-   
-    if [ -e "$DATAFILE" ]; then
+    ## TODO: Improve this by utilising a query with tags
+    if [ $(grep 'VPCID' $DATAFILE) != '' ]; then
         echo 'VPC DATA EXISTS!!'
         exit 255
     fi
-
-    touch "$DATAFILE"
 
     # Create a VPC with a 10.0.0.0/16 CIDR block
     VPCID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 \
@@ -100,7 +58,7 @@ create_vpc () {
         | tr -d ',"')
 
     # Give the servicdiscovery service time to process
-    sleep 20
+    sleep 30
 
     NAMESPACEID=$(aws servicediscovery get-operation \
         --operation-id "$OPERATIONID" \
@@ -110,22 +68,22 @@ create_vpc () {
 
     echo "NAMESPACEID $NAMESPACEID" >> "$DATAFILE"
 
-    # Create a subnet with 10.0.1.0/24 CIDR block
+    # Create a subnet with 10.0.0.0/24 CIDR block
     SUBNETID0=$(aws ec2 create-subnet \
         --availability-zone "$REGION"a \
         --vpc-id "$VPCID" \
-        --cidr-block 10.0.1.0/24 \
+        --cidr-block 10.0.0.0/24 \
         | grep 'SubnetId' \
         | awk '{ print $2 }' \
         | tr -d ',"')
 
     echo "SUBNETID0 $SUBNETID0" >> "$DATAFILE"
 
-# Create a subnet with 10.0.0.0/24 CIDR block
+    # Create a subnet with 10.0.1.0/24 CIDR block
     SUBNETID1=$(aws ec2 create-subnet \
         --availability-zone "$REGION"b \
         --vpc-id "$VPCID" \
-        --cidr-block 10.0.0.0/24 \
+        --cidr-block 10.0.1.0/24 \
         | grep 'SubnetId' \
         | awk '{ print $2 }' \
         | tr -d ',"')
@@ -187,7 +145,7 @@ create_vpc () {
 create_ecrRepos () {
 
     for i in {app,db,webserver}; do
-        REPOSITORYUI=$(aws ecr create-repository \
+        REPOSITORYURI=$(aws ecr create-repository \
         --repository-name "$PROJECT/$i" \
         --image-scanning-configuration scanOnPush=false \
         --region "$REGION" \
@@ -195,8 +153,128 @@ create_ecrRepos () {
         | awk '{ print $2 }' \
         | tr -d ',"')
 
-        sed -i "#image# s#: .*# :\"$REPOSITORYUI\",#" tasks/$i-task.json
+        ## Populating the *-task.json from the *-task.json.tmpl
+        sed "#image# s#: .*# :\"$REPOSITORYURI\",#" \
+            ecs/tasks/$i-task.json.yml > ecs/tasks/$i-task.json
     done
+
+}
+
+create_cloudWatchLogGroups() {
+
+    for i in {app,db,webserver}; do
+        aws logs create-log-group \
+            --log-group-name /ecs/"$PROJECT"-"$i"
+    done
+
+    aws logs create-log-group \
+        --log-group-name /codebuild/"$PROJECT"
+
+}
+
+create_artifactS3Bucket () {
+
+    ## TODO: Randomize the bucket name
+    aws s3 mb s3://codepipeline-eu-west-2-"$PROJECT"
+
+}
+
+create_efs () {
+
+    FILESYSTEMID=$(aws efs create-file-system \
+        --no-encrypted \
+        --region "$REGION" \
+        | grep 'FileSystemId' | awk '{ print $2 }' \
+        | tr -d ',"')
+
+    echo "FILESYSTEMID $FILESYSTEMID" >> "$DATAFILE"
+
+    ## Update the task definition for the db to consume this EFS
+    sed -i "s/<FILESYSTEMID>/$FILESYSTEMID/g" ecs/tasks/db-task.json
+
+    # Give it time to become available
+    sleep 30
+
+    # Create a Mount Target
+    MOUNTTARGETID0=$(aws efs create-mount-target \
+        --file-system-id "$FILESYSTEMID" \
+        --subnet-id "$SUBNETID0" \
+        --security-group "$SECURITYGROUPID" \
+        --region "$REGION" \
+        | grep 'MountTargetId' | awk '{ print $2 }' \
+        | tr -d ',"')
+
+    echo "MOUNTTARGETID0 $MOUNTTARGETID0" >> "$DATAFILE"
+
+    MOUNTTARGETID1=$(aws efs create-mount-target \
+        --file-system-id "$FILESYSTEMID" \
+        --subnet-id "$SUBNETID1" \
+        --security-group "$SECURITYGROUPID" \
+        --region "$REGION" \
+        | grep 'MountTargetId' | awk '{ print $2 }' \
+        | tr -d ',"')
+
+    echo "MOUNTTARGETID1 $MOUNTTARGETID1" >> "$DATAFILE"
+
+}
+
+create_ecsCluster () {
+
+    if [ $(grep 'CLUSTER' $DATAFILE) != '' ]; then
+        echo 'CLUSTER DATA EXISTS!!'
+        exit 255
+    fi
+
+    CLUSTERNAME="$PROJECT"-cluster
+    echo "CLUSTERNAME $CLUSTERNAME" >> "$DATAFILE"
+
+    CLUSTERARN=$(aws ecs create-cluster --cluster-name "$CLUSTERNAME" \
+        | grep 'clusterArn' | awk '{ print $2 }' | tr -d ',"')
+
+    echo "CLUSTERARN $CLUSTERARN" >> "$DATAFILE"
+
+}
+
+create_ecsTaskExecutionRole () {
+
+    local ROLENAME="ecsTaskExecutionRole"
+    local ROLEARN=''
+        ROLEARN=$(aws iam create-role \
+        --region "$REGION" \
+        --role-name "$ROLENAME" \
+        --assume-role-policy-document file://ecs/ecs-task-execution-assume-role.json \
+        | grep 'Arn' \
+        | awk '{ print $2 }' \
+        | tr -d ',"' )
+
+    for i in {app,db,webserver}; do
+        sed -i "#executionRoleArn# s#: .*# :\"$ROLEARN\",#" ecs/tasks/$i-task.json
+     done
+
+    # Attach the role policy to the role
+    aws iam attach-role-policy \
+    --region "$REGION" \
+    --role-name "$ROLENAME" \
+    --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
+
+}
+
+create_ecsTaskRegistrations () {
+
+    for i in {app,db,webserver}; do
+        sed -i "s/<ACCOUNT>/$ACCOUNT/g" ecs/tasks/"$i"-task.json
+        sed -i "s/<PROJECT>/$PROJECT/g" ecs/tasks/"$i"-task.json
+
+        # register the task definition
+        aws ecs register-task-definition \
+            --cli-input-json file://ecs/tasks/"${i}"-task.json
+    done
+
+    ## TODO: WIP
+    # Update the task definition for code deploy
+    #for i in {app,db,webserver}; do
+    #    sed -i "/image/s/$i/<IMAGE1_NAME>/" ecs/tasks/"$i"-task.json
+    #done
 
 }
 
@@ -220,15 +298,23 @@ create_codeBuildServiceRole () {
     aws iam attach-role-policy \
         --role-name CodeBuildServiceRole \
         --policy-arn arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds
-}
-
-create_codeBuildProject () {
 
     # Give the permissions time to apply
     sleep 20
 
+}
+
+create_codeBuildProject () {
+
+    ## Populate the project.json from the project.json.tmpl
+    sed "s/<PROJECT>/$PROJECT/g" codeBuild/project.json.tmpl \
+        > codeBuild/project.json
+
+    sed -i "s/<REGION>/$REGION/g" codeBuild/project.json
+    sed -i "s/<ACCOUNT>/$ACCOUNT/g" codeBuild/project.json
+
     aws codebuild create-project \
-        --cli-input-json file://codeBuild/"$PROJECT"-project.json
+        --cli-input-json file://codeBuild/project.json
 
     ## Run the build to populate the ECR repos
     aws codebuild start-build \
@@ -237,114 +323,15 @@ create_codeBuildProject () {
 
 }
 
-create_ecsCluster () {
-
-    CLUSTERNAME="$PROJECT"-cluster
-    local DATAFILE="cluster.data"
-
-    if [ -e "$DATAFILE" ]; then
-        echo 'CLUSTER DATA EXISTS!!'
-        exit 255
-    fi
-
-    touch "$DATAFILE"
-
-    CLUSTERARN=$(aws ecs create-cluster --cluster-name "$CLUSTERNAME" \
-        | grep 'clusterArn' | awk '{ print $2 }' | tr -d ',"')
-
-        echo "CLUSTERNAME $CLUSTERNAME" >> "$DATAFILE"
-        echo "CLUSTERARN $CLUSTERARN" >> "$DATAFILE"
-
-}
-
-create_efs () {
-
-    local DATAFILE="efs.data"
-    FILESYSTEMID=$(aws efs create-file-system \
-        --no-encrypted \
-        --region "$REGION" \
-        | grep 'FileSystemId' | awk '{ print $2 }' \
-        | tr -d ',"')
-
-    # Give it time to become available
-    sleep 30
-
-    # Create a Mount Target
-    MOUNTTARGETID0=$(aws efs create-mount-target \
-        --file-system-id "$FILESYSTEMID" \
-        --subnet-id "$SUBNETID0" \
-        --security-group "$SECURITYGROUPID" \
-        --region "$REGION" \
-        | grep 'MountTargetId' | awk '{ print $2 }' \
-        | tr -d ',"')
-
-    MOUNTTARGETID1=$(aws efs create-mount-target \
-        --file-system-id "$FILESYSTEMID" \
-        --subnet-id "$SUBNETID1" \
-        --security-group "$SECURITYGROUPID" \
-        --region "$REGION" \
-        | grep 'MountTargetId' | awk '{ print $2 }' \
-        | tr -d ',"')
-
-    ## Update the task definition for the db to consume this EFS
-    sed -i "s/<FILESYSTEMID>/$FILESYSTEMID/g" tasks/db-task.json
-
-    echo "FILESYSTEMID $FILESYSTEMID" > "$DATAFILE"
-    echo "MOUNTTARGETID0 $MOUNTTARGETID0" >> "$DATAFILE"
-    echo "MOUNTTARGETID1 $MOUNTTARGETID1" >> "$DATAFILE"
-
-}
-
-create_ecsTaskExecutionRole () {
-
-    local ROLENAME="ecsTaskExecutionRole"
-    local ROLEARN=''
-        ROLEARN=$(aws iam create-role \
-        --region "$REGION" \
-        --role-name "$ROLENAME" \
-        --assume-role-policy-document file://ecs-task-execution-assume-role.json \
-        | grep 'Arn' \
-        | awk '{ print $2 }' \
-        | tr -d ',"' )
-
-    for i in {app,db,webserver}; do
-        sed -i "#executionRoleArn# s#: .*# :\"$ROLEARN\",#" tasks/$i-task.json
-     done
-
-    # Attach the role policy to the role
-    aws iam attach-role-policy \
-    --region "$REGION" \
-    --role-name "$ROLENAME" \
-    --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
-
-}
-
-create_ecsTaskRegistrations () {
-
-    aws ecs register-task-definition --cli-input-json file://tasks/app-task.json
-    aws ecs register-task-definition --cli-input-json file://tasks/webserver-task.json
-    aws ecs register-task-definition --cli-input-json file://tasks/db-task.json
-
-    # Update the task definition for code deploy
-    for i in {app,db,webserver}; do
-        sed -i "/image/s/$i/<IMAGE1_NAME>/" tasks/"$i"-task.json
-    done
-
-}
-
-create_cloudwatchLogGroups() {
-
-    for i in {app,db,webserver}; do
-        aws logs create-log-group \
-            --log-group-name /ecs/"$PROJECT"-"$i"
-    done
-
-    aws logs create-log-group \
-        --log-group-name /codebuild/"$PROJECT"
-
-}
-
 create_codePipelineServiceRole () {
+
+    ## Populate the .json from the .json.tmpl
+    sed "s/<ACCOUNT>/$ACCOUNT/g" \
+        codePipeline/create-role-policy.json.tmpl \
+        > codePipeline/create-role-policy.json
+
+    sed -i "s/<PROJECT>/$PROJECT/g" \
+        codePipeline/create-role-policy.json
 
     aws iam create-role \
         --role-name CodePipelineServiceRole \
@@ -360,58 +347,23 @@ create_codePipelineServiceRole () {
         --policy-name CodePipelineServiceRolePolicy \
         --policy-document file://codePipeline/create-role-policy.json
 
-
-}
-
-create_artifactS3Bucket () {
-
-    aws s3 mb s3://codepipeline-eu-west-2-"$PROJECT"
-
-}
-
-create_codePipeline () {
-
-    sleep 20
-    aws codepipeline create-pipeline \
-        --cli-input-json file://codePipeline/create-pipeline.json
-
-}
-
-create_ecsServices () {
-
-    REGISTRYARN=$(aws servicediscovery list-namespaces \
-        | grep 'Arn' | awk '{ print $2 }' | tr -d ',"')
-
-    for i in {app,db,webserver}; do
-        sed "s/<FAMILY>/$PROJECT-$i/" services/create-"$i"-service.json.tmpl \
-            > services/create-"$i"-service.json
-        sed -i "s/<CLUSTERNAME>/$CLUSTERNAME/" services/create-"$i"-service.json
-        sed -i "s#<REGISTRYARN>#$REGISTRYARN#" services/create-"$i"-service.json
-    done
-
-    sed -i "s#<TARGETGROUPARN>#$TARGETGROUPARN#" \
-        services/create-webserver-service.json
-    sed -i "s/<CONTAINERNAME>/$PROJECT-webserver/" \
-        services/create-webserver-service.json
-    sed -i "s/<SUBNETID0>/$SUBNETID0/" \
-        services/create-webserver-service.json
-    sed -i "s/<SUBNETID1>/$SUBNETID1/" \
-        services/create-webserver-service.json
-    sed -i "s/<SECURITYGROUPID>/$SECURITYGROUPID/" \
-        services/create-webserver-service.json
-
-    for i in {app,db,webserver}; do
-        aws ecs create-service \
-            --service-name "$i"-service \
-            --cli-input-json file://services/create-"$i"-service.json
-    done
-
 }
 
 create_loadbalancer () {
-    
+
+    ## Need two load balancers, public and private facing
+    ## Public facing load balancer resolves traffic on port 80 to the webserver
+    ## Private facing load balancer resolves traffic on port 3306 to the mysqldb
+    ## and traffic on 9000 to the php app server.
+
+    ## Requires two target groups for each of the services {app,db,webservice} in
+    ## order to facilitate the code deploy deployments for each component.\
+
+    ## Listeners for the private load balancer endpoints (3306 and 9000) require
+    ## private DNS resolution (app.local, db.local)
+
     LOADBALANCERARN=$(aws elbv2 create-load-balancer \
-        --name "$PROJECT"-alb \
+        --name "$PROJECT"-public-alb \
         --scheme internet-facing \
         --type application \
         --subnets "$SUBNETID0" "$SUBNETID1" \
@@ -449,6 +401,62 @@ create_loadbalancer () {
 
 }
 
+create_ecsServices () {
+
+    REGISTRYARN=$(aws servicediscovery list-namespaces \
+        | grep 'Arn' | awk '{ print $2 }' | tr -d ',"')
+
+    for i in {app,db,webserver}; do
+        sed "s/<FAMILY>/$PROJECT-$i/" services/create-"$i"-service.json.tmpl \
+            > services/create-"$i"-service.json
+        sed -i "s/<CLUSTERNAME>/$CLUSTERNAME/" services/create-"$i"-service.json
+        sed -i "s#<REGISTRYARN>#$REGISTRYARN#" services/create-"$i"-service.json
+    done
+
+    sed -i "s#<TARGETGROUPARN>#$TARGETGROUPARN#" \
+        services/create-webserver-service.json
+    sed -i "s/<CONTAINERNAME>/$PROJECT-webserver/" \
+        services/create-webserver-service.json
+    sed -i "s/<SUBNETID0>/$SUBNETID0/" \
+        services/create-webserver-service.json
+    sed -i "s/<SUBNETID1>/$SUBNETID1/" \
+        services/create-webserver-service.json
+    sed -i "s/<SECURITYGROUPID>/$SECURITYGROUPID/" \
+        services/create-webserver-service.json
+
+    for i in {app,db,webserver}; do
+        aws ecs create-service \
+            --service-name "$i"-service \
+            --cli-input-json file://services/create-"$i"-service.json
+    done
+
+    sleep 20
+}
+
+create_codePipeline () {
+
+    ## Populate the create-pipeline.json from the create-pipeline.json.tmpl
+    sed "s/<ACCOUNT>/$ACCOUNT/g" \
+        codePipeline/create-pipeline.json.tmpl \
+        > codePipeline/create-pipeline.json
+    sed -i "s/<PROJECT>/$PROJECT/g" \
+        codePipeline/create-pipeline.json
+
+    ## Determine Codestar integration
+    CONNECTIONARN=$(aws codestar-connections \
+        list-connections \
+        | grep 'ConnectionArn' \
+        | awk '{ print $2 }' \
+        | tr -d ',"')
+
+    sed -i "s#<CONNECTIONARN>#$CONNECTIONARN#g" codePipeline/create-pipeline.json
+
+    # Create the codePipeline
+    aws codepipeline create-pipeline \
+        --cli-input-json file://codePipeline/create-pipeline.json
+
+}
+
 create_codeDeployServiceRole () {
     ## TODO - WIP
     aws iam create-role --role-name CodeDeployServiceRole \
@@ -459,14 +467,12 @@ create_codeDeployServiceRole () {
 
 }
 
-
-
 main () {
-    ## TODO Populate the docker credentials within secrets manager
+    ## TODO Populate docker credentials
     init
     create_vpc
     create_ecrRepos
-    create_cloudwatchLogGroups
+    create_cloudWatchLogGroups
     create_artifactS3Bucket
     create_efs
     create_ecsCluster
